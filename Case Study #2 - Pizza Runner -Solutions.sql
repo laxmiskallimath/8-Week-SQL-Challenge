311fa54a47ebd4d22810757df28a25acb85cda15
Laxmi S Kallimath <38729902+laxmiskallimath@users.noreply.github.com>
2024-02-05 23:16:01 +0530

    Update Case Study #2 - Pizza Runner -Solutions.sql

--- Case Study #2 - Pizza Runner -Solutions.sql
+++ Case Study #2 - Pizza Runner -Solutions.sql
@@ -1,6 +1,19 @@
+-- USE DATABASE
 use eight_week_sql_challenge;
 
--- create runners table 
+-- DROP SCHEMA IF EXISTS 
+DROP SCHEMA pizza_runner;
+
+-- CREATE SCHEMA
+create schema pizza_runner;
+
+-- USE SCHEMA 
+USE pizza_runner;
+
+-- Create runners table which shows the registration_date for each new runner 
+
+DROP TABLE IF EXISTS runners;
+
 CREATE TABLE runners (
   runner_id INTEGER,
   registration_date DATE
@@ -16,7 +29,8 @@ VALUES
   
   select * from runners;
   
--- create customer_orders
+-- Customer pizza orders are captured in the customer_orders table with 1 row for each individual pizza that is part of the order.
+
 CREATE TABLE customer_orders (
   order_id INTEGER,
   customer_id INTEGER,
@@ -46,7 +60,11 @@ VALUES
   
 select * from customer_orders;
 
--- CREATE TABLE runner_orders
+-- After each orders are received through the system - they are assigned to a runner - however not all orders are fully completed and 
+-- can be cancelled by the restaurant or the customer.
+
+DROP TABLE IF EXISTS runner_orders;
+
 CREATE TABLE runner_orders (
   order_id INTEGER,
   runner_id INTEGER,
@@ -72,7 +90,9 @@ VALUES
 
 select * from runner_orders;
 
--- CREATE TABLE pizza_names
+-- Pizza Runner only has 2 pizzas available the Meat Lovers or Vegetarian! which is captured in pizza_names table
+DROP TABLE IF EXISTS pizza_names;
+
  CREATE TABLE pizza_names (
   pizza_id INTEGER,
   pizza_name TEXT
@@ -85,6 +105,9 @@ VALUES
   (2, 'Vegetarian');
   
 select * from pizza_names;
+
+-- Each pizza_id has a standard set of toppings which are used as part of the pizza recipe.These details are captured in the pizza_recieps table
+DROP TABLE IF EXISTS pizza_recipes;
 
 -- CREATE TABLE pizza_recipes
 CREATE TABLE pizza_recipes (
@@ -99,6 +122,9 @@ VALUES
   (2, '4, 6, 7, 9, 11, 12');
 
 select * from pizza_recipes;
+
+-- Toppings table contains all of the topping_name values with their corresponding topping_id value
+DROP TABLE IF EXISTS pizza_toppings;
 
 CREATE TABLE pizza_toppings (
   topping_id INTEGER,
@@ -132,109 +158,131 @@ VALUES
 	File Name: pizza_runner_solutions.sql
 */
 
-/*Lets Clean Data :The customer_order table has inconsistent data types.  We must first clean the data before answering any questions.
+/*Lets Clean Data :
+The customer_order table has inconsistent data types.
+We must first clean the data before answering any questions.
 The exclusions and extras columns contain values that are either 'null' (text), null (data type) or '' (empty).
 We will create a temporary table where all forms of null will be transformed to NULL (data type).*/
 
 -- The source data
 select * from customer_orders;
 
-create table cleaned_customers_data as(
-                  SELECT 
-                        order_id,
-                        customer_id,
-                        pizza_id,
-					    CASE 
-                             WHEN exclusions = '' OR exclusions = 'null' OR exclusions = 'NaN' THEN NULL
-                             ELSE exclusions
-					    END AS exclusions,
-                        CASE 
-                            WHEN  extras = '' OR  extras = 'null' OR extras = 'NaN' THEN NULL 
-						    ELSE extras
-						END AS extras,
-					   order_time
-				 FROM customer_orders
-);
+DROP TABLE IF EXISTS customer_orders_temp;
 
-select * from cleaned_customers_data;
+CREATE TEMPORARY TABLE customer_orders_temp AS
+SELECT  order_id,
+        customer_id,
+        pizza_id,
+        CASE  
+            WHEN exclusions = '' THEN null
+            WHEN exclusions = 'null' THEN NULL
+            ELSE exclusions
+		END AS exclusions,
+        CASE 
+            WHEN extras = '' THEN NULL
+            WHEN extras = 'null' THEN NULL
+            ELSE extras 
+		END as extras,
+        order_time
+FROM customer_orders;
 
--- Similarly customers_orders data The runner_order table has inconsistent data types. So lets clean runnerWe must first clean the data before answering any questions. 
--- The distance and duration columns have text like km and numbers hence will remove the text values and convert to numeric values .  
--- We will convert all 'null' (text) and 'NaN' values in the cancellation column to null (data type).
+SELECT * FROM customer_orders_temp;
+
+-- *** Clean Data ***--------------------------
+-- The runner_order table has inconsistent data types. 
+-- We must first clean the data before answering any questions. 
+-- The distance and duration columns have text and numbers.
+-- We will remove the text values and convert to numeric values.
+-- We will convert all 'null' (text) and 'NaN' values in the cancellation column to NULL (data type).
 -- We will convert the pickup_time (varchar) column to a timestamp data type.
+
+-- The orginal table structure.
+SELECT * FROM pizza_runner.runner_orders;
 
 -- original data
 select * from runner_orders;
 
-SELECT
-  table_name,
-  column_name,
-  data_type
-FROM information_schema.columns
-WHERE table_name = 'runner_orders';
+DROP TABLE IF EXISTS runner_orders_temp;
 
-CREATE TABLE cleaned_runner_orders AS
-SELECT 
-  order_id, 
-  runner_id,  
-  CASE
-	  WHEN pickup_time LIKE 'null' THEN ' '
-	  ELSE pickup_time
-	  END AS pickup_time,
-  CASE
-	  WHEN distance LIKE 'null' THEN ' '
-	  WHEN distance LIKE '%km' THEN TRIM('km' from distance)
-	  ELSE distance 
-    END AS distance,
-  CASE
-	  WHEN duration LIKE 'null' THEN ' '
-	  WHEN duration LIKE '%mins' THEN TRIM('mins' from duration)
-	  WHEN duration LIKE '%minute' THEN TRIM('minute' from duration)
-	  WHEN duration LIKE '%minutes' THEN TRIM('minutes' from duration)
-	  ELSE duration
-	  END AS duration,
-  CASE
-	  WHEN cancellation IS NULL or cancellation LIKE 'null' THEN ' '
-	  ELSE cancellation
-	  END AS cancellation
-FROM runner_orders;
+CREATE TEMPORARY TABLE runner_orders_temp AS
 
-SELECT * FROM cleaned_runner_orders;
+SELECT order_id,
+       runner_id,
+       CASE 
+          WHEN pickup_time LIKE 'null' THEN NULL
+		  ELSE pickup_time
+	   END AS pickup_time,
+       CASE 
+          WHEN distance LIKE 'null' THEN NULL 
+          ELSE CAST(regexp_replace(distance,'[a-z]+','') AS FLOAT)
+	   END AS distance,
+       CASE 
+           WHEN duration LIKE 'null' THEN NULL 
+           ELSE CAST(regexp_replace(duration,'[a-z]+','') AS FLOAT)
+	   END AS duration,
+       CASE 
+           WHEN cancellation LIKE '' THEN NULL 
+           WHEN cancellation LIKE 'null' THEN NULL
+           ELSE cancellation
+	   END AS cancellation
+	FROM runner_orders;
+       
+SELECT * FROM runner_orders_temp; 
 
--- WHEN I TRIED MODIFY DATATPE OF PICKUP TIME USING ALTER COMMAND I WAS GETTING 
---  ERROR 1292 (22007): Incorrect datetime value: '' for column 'pickup_time' at row 6
--- to resolve this issue in mysql i used below command
-SET SQL_MODE='ALLOW_INVALID_DATES';
+-- pizza_recipes table The toppings column in the pizza_recipes table is a comma separated string.
+-- Using JSON functions : These functions are used to split the comma separated string into multiple rows.
+-- json_array() converts the string to a JSON array
+-- We enclose array elements with double quotes, this is performed using the replace function and we trim the resultant array
+SELECT *,
+       json_array(toppings),
+       replace(json_array(toppings),',','","'),
+       trim(replace(json_array(toppings),',','","'))
+FROM pizza_runner.pizza_recipes;
 
+-- We convert the json data into a tabular data using json_table().
+-- Syntax: JSON_TABLE(expr, path COLUMNS (column_list) [AS] alias)
+-- It extracts data from a JSON document and returns it as a relational table having the specified columns
+-- Each match for the path preceding the COLUMNS keyword maps to an individual row in the result table.
+ -- The expression "$[*]" matches each element of the array and maps it to an individual row in the result table.
+-- columns (topping varchar(50) PATH '$') -- Within a column definition, "$" passes the entire match to the column; 
 
-ALTER TABLE cleaned_runner_orders MODIFY COLUMN  pickup_time DATETIME;
--- ALTER TABLE runner_orders_temp MODIFY COLUMN  pickup_time datetime  NULL DEFAULT '1970-01-01 00:00:00';
-ALTER TABLE cleaned_runner_orders MODIFY COLUMN  distance FLOAT;
-ALTER TABLE cleaned_runner_orders MODIFY COLUMN  duration int;
+SELECT t.pizza_id, (j.topping)
+FROM pizza_recipes t
+JOIN json_table(trim(replace(json_array(t.toppings), ',', '","')), '$[*]' columns (topping varchar(50) PATH '$')) j ;
 
-select sum(duration) from cleaned_runner_orders;
+-- customer_orders_temp table
+-- The exclusions and extras columns in the pizza_recipes table are comma separated strings.
+SELECT t.order_id,
+       t.customer_id,
+       t.pizza_id,
+       trim(j1.exclusions) AS exclusions,
+       trim(j2.extras) AS extras,
+       t.order_time
+FROM customer_orders_temp t
+INNER JOIN json_table(trim(replace(json_array(t.exclusions), ',', '","')), '$[*]' columns (exclusions varchar(50) PATH '$')) j1
+INNER JOIN json_table(trim(replace(json_array(t.extras), ',', '","')), '$[*]' columns (extras varchar(50) PATH '$')) j2 ;
 
-select sum(distance) from cleaned_runner_orders;
-
-SELECT
-  table_name,
-  column_name,
-  data_type
-FROM information_schema.columns
-WHERE table_name = 'cleaned_runner_orders';
+-- 🍕 Case Study #2: Pizza runner - Pizza Metrics
+-- Case Study Questions
 
 -- 1. How many pizzas were ordered?
-SELECT 
-      COUNT(*) AS no_of_pizza_ordered_count
-FROM 
-	cleaned_customers_data;
--- no_of_pizza_ordered_count : 14    
+SELECT count(pizza_id) AS "Total Number Of Pizzas Ordered"
+FROM pizza_runner.customer_orders;
+
+/*
+
+Results:
+
+number_of_orders|
+----------------+
+              14|
+      
+*/
 
 -- 2. How many unique customer orders were made?
 SELECT 
-  COUNT(DISTINCT order_id) AS unique_order_count
-FROM
-    cleaned_customers_data;
+  COUNT(DISTINCT order_id) AS 'Number Of Unique Orders'
+FROM customer_orders_temp;
 
 /*
 
@@ -242,20 +290,13 @@ unique_orders|
 -------------+
            10|      
 */
-
 -- 3. How many successful orders were delivered by each runner?
-select 
-      runner_id,
-      count(order_id) as successful_orders
-from 
-      cleaned_runner_orders
-where 
-      cancellation IS NULL OR cancellation NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
-GROUP BY
-	   runner_id
-ORDER BY
-	successful_orders DESC;
--- RESULT    
+SELECT runner_id,
+       count(order_id) AS 'Number Of Successful Orders'
+FROM pizza_runner.runner_orders_temp
+WHERE cancellation IS NULL
+GROUP BY runner_id;
+
 /*
 
 runner_id|successful_orders|
@@ -267,74 +308,71 @@ runner_id|successful_orders|
 */
 
 -- 4. How many of each type of pizza was delivered?
--- pizza_names
+SELECT pizza_id,
+       pizza_name,
+       count(pizza_id) AS 'Number Of Pizzas Delivered'
+FROM pizza_runner.runner_orders_temp
+INNER JOIN customer_orders_temp USING (order_id)
+INNER JOIN pizza_names USING (pizza_id)
+WHERE cancellation IS NULL
+GROUP BY pizza_id;
+
+-- OR 
+
 SELECT 
-      PIZZA_NAME ,
-      COUNT(*) AS PIZZA_TYPE_COUNT
+    T2. pizza_id,
+     pizza_name,
+     count(T2.pizza_id) AS Number_of_Pizzas_Delivered
 FROM 
-     cleaned_customers_data AS T1
+	customer_orders_temp AS T1
 INNER JOIN 
      pizza_names AS T2
-ON  
-     t2.pizza_id  = t1.pizza_id
-INNER JOIN 
-     cleaned_runner_orders AS T3
 ON 
-    T1.order_id = T3.order_id
+    T2.pizza_id = T1.pizza_id
+INNER JOIN 
+    runner_orders_temp AS T3
+ON 
+   T1.order_id = T3.order_id
 WHERE 
-     cancellation IS NULL OR cancellation NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
+  cancellation IS NULL
 GROUP BY 
-       T2.pizza_name 
-ORDER BY 
-      PIZZA_TYPE_COUNT DESC;
-
--- RESULT
+  T2.pizza_name
+ORDER BY Number_of_Pizzas_Delivered DESC;
+     
 /*
-
 pizza_name|delivery_count|
 ----------+--------------+
 Meatlovers|             9|
 Vegetarian|             3|  
-            
-*/   
+*/     
 
-
-SELECT 
-      PIZZA_NAME ,
-      COUNT(*) AS PIZZA_TYPE_COUNT
-FROM 
-     cleaned_customers_data AS T1
-INNER JOIN 
-     pizza_names AS T2
-ON  
-     t2.pizza_id  = t1.pizza_id
-GROUP BY 
-       T2.pizza_name 
-ORDER BY 
-      PIZZA_TYPE_COUNT DESC;
-      
 -- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT DISTINCT customer_id,
+       pizza_name,
+       count(pizza_id) AS 'Number Of Pizzas Ordered'
+FROM customer_orders_temp
+INNER JOIN pizza_names USING (pizza_id)
+GROUP BY  customer_id,
+         pizza_id
+ORDER BY customer_id ;
+
+-- OR
+
 SELECT 
       customer_id,
-      SUM(
-           CASE 
-               WHEN pizza_id = 1 THEN 1
-               ELSE 0
-			END 
-            )AS Meatlovers,
-            
-	  SUM(
-           CASE 
-               WHEN pizza_id = 2 THEN 1
-               ELSE 0 
-		  END 
-          ) AS Vegetarian
-FROM 
-     cleaned_customers_data
-GROUP BY 
-       customer_id 
-ORDER BY 
-       customer_id;
+	  SUM(CASE
+              WHEN pizza_id = 1 THEN 1
+              ELSE 0
+		   END) AS  meat_lovers,
+	  SUM(CASE
+              WHEN pizza_id = 2 THEN 1 
+              ELSE 0 
+           END) AS vegetarian
+FROM customer_orders_temp
+GROUP BY customer_id 
+ORDER BY customer_id;
+         
  -- RESULT 
   /*
 
@@ -348,97 +386,55 @@ customer_id|meat_lovers|vegetarian|
             
 */
         
-  
-  -- or 
-  SELECT 
-  T2.customer_id, 
-  T1.pizza_name, 
-  COUNT(T1.pizza_name) AS order_count
-FROM pizza_names AS T1
-JOIN cleaned_customers_data AS T2
-  ON T2.pizza_id= T1.pizza_id
-GROUP BY T2.customer_id, T1.pizza_name
-ORDER BY T2.customer_id;
-
 -- 6. What was the maximum number of pizzas delivered in a single order?  
-WITH order_count_cte as (
-  SELECT 
-      T1.order_id,
-      COUNT(T1.pizza_id) AS n_orders
-FROM 
-     cleaned_customers_data AS T1
-INNER JOIN 
-      cleaned_runner_orders AS T2
-ON 
-      T1.order_id = T2.order_id
-WHERE 
-     T2.cancellation IS NULL or cancellation  NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
-GROUP  BY 
-      T1.order_id
- )     
 SELECT 
-       MAX(n_orders) AS maximum_number_of_pizzas_delivered
-FROM order_count_cte;
+      customer_id,
+      order_id,
+      count(order_id) AS pizza_count
+FROM 
+     customer_orders_temp
+GROUP BY 
+     order_id
+ORDER BY 
+     pizza_count DESC
+LIMIT 1;
 
 -- OR 
-SELECT
-       MAX(pizza_count) as max_count
-FROM
-	(SELECT 
-          T1. order_id ,
-           count(T1.pizza_id) AS pizza_count
-     FROM 
-     cleaned_customers_data AS T1
-     JOIN 
-    cleaned_runner_orders AS T2
-     ON 
-    T1.order_id = T2.order_id 
-     WHERE 
-       T2.cancellation IS NULL
-    OR T2 .cancellation NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
-    GROUP BY T1.order_id )  AS MAX_ORDER_COUNT; 
--- RESULT
-   /*
-
-max_delivered_pizzas|
---------------------+
-                   3|  
-            
-*/
+SELECT 
+      MAX(pizza_count) as max_count 
+FROM 
+     (SELECT 
+            T1.order_id,
+            count(T1.pizza_id) as pizza_count
+	  FROM 
+          customer_orders_temp AS T1
+	  INNER JOIN 
+           runner_orders_temp AS T2 
+	  ON 
+      T1.order_id  = T2.order_id
+      WHERE 
+	  T2.cancellation IS NULL  OR  T2.cancellation  NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
+      GROUP BY T1.order_id) as MAX_ORDER_COUNT ;
 
 -- 7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- at least 1 change -> either exclusion or extras
+-- no changes -> exclusion and extras are NULL
+SELECT customer_id,
+      SUM(CASE 
+              WHEN (exclusions IS NOT NULL OR extras IS NOT NULL ) THEN 1
+              ELSE 0 
+		 END ) AS change_in_pizza,
+	  SUM(CASE 
+              WHEN (exclusions IS NULL AND extras  IS NULL) THEN 1 
+		  ELSE 0 
+          END ) AS  no_change_in_pizza
+FROM customer_orders_temp
+INNER JOIN  runner_orders_temp using (order_id)
+WHERE cancellation IS NULL
+GROUP BY customer_id
+ORDER BY  customer_id;
 
-SELECT 
-      customer_id ,
-      SUM(
-            CASE 
-                WHEN  T1.exclusions IS NOT NULL OR T1.extras IS NOT NULL THEN 1
-                ELSE 0 
-			END 
-		) AS with_one_change,
-		SUM(
-            CASE 
-                WHEN  T1.exclusions IS NULL AND  T1.extras IS NULL THEN 1
-                ELSE 0 
-			END 
-		) AS no_changes
-FROM 
-     cleaned_customers_data AS T1
-JOIN 
-     cleaned_runner_orders AS T2
-ON 
-    T1.order_id = T2.order_id
-WHERE 
-    T2.cancellation IS NULL 
-     OR T2.cancellation NOT IN ('Restaurant Cancellation', 'Customer Cancellation')
-GROUP BY
-     T1.customer_id
-ORDER BY
-      T1.customer_id;
-      
--- RESULT
 /*
-
 customer_id|with_changes|no_changes|
 -----------+------------+----------+
         101|           0|         2|
@@ -446,48 +442,42 @@ customer_id|with_changes|no_changes|
         103|           3|         0|
         104|           2|         1|
         105|           1|         0|  
-            
 */
 
--- 8. How many pizzas were delivered that had both exclusions and extras?        
 
-SELECT
-	SUM(
-		CASE
-			WHEN T1.exclusions IS NOT NULL AND t1.extras IS NOT NULL THEN 1
-			ELSE 0
-		END
-	) AS number_of_pizzas
-FROM 
-	cleaned_customers_data AS T1
-JOIN 
-	cleaned_runner_orders AS T2
-ON 
-	T1.order_id = T2.order_id
-WHERE 
-	T2.cancellation IS NULL
-	OR T2.cancellation NOT IN ('Restaurant Cancellation', 'Customer Cancellation');
-
-/*
-
-number_of_pizzas|
-----------------+
-               1|  
-            
-*/      
-
+-- 8. How many pizzas were delivered that had both exclusions and extras?                      
+SELECT customer_id,
+       SUM(CASE
+               WHEN (exclusions IS NOT NULL
+                     AND extras IS NOT NULL) THEN 1
+               ELSE 0
+           END) AS both_change_in_pizza
+FROM customer_orders_temp
+INNER JOIN runner_orders_temp USING (order_id)
+WHERE cancellation IS NULL
+GROUP BY customer_id
+ORDER BY customer_id;
+    
 -- 9. What was the total volume of pizzas ordered for each hour of the day?
+SELECT hour(order_time) AS 'Hour',
+       count(order_id) AS 'Number of pizzas ordered',
+       round(100*count(order_id) /sum(count(order_id)) over(), 2) AS 'Volume of pizzas ordered'
+FROM pizza_runner.customer_orders_temp
+GROUP BY 1
+ORDER BY 1;
+
+-- OR 
 SELECT 
       HOUR(order_time) AS hour_of_day, 
 	  COUNT(order_id) AS pizza_count
 FROM 
-      cleaned_customers_data
+      customer_orders_temp
 GROUP BY 
       HOUR(order_time)
 ORDER  BY 
        HOUR(order_time);
- 
--- Result:
+       
+ -- Result:
 //*+──────────────+──────────────+
 | hour_of_day  | pizza_count  |
 +──────────────+──────────────+
@@ -500,11 +490,22 @@ ORDER  BY
 | 23           | 1            |
 +──────────────+──────────────+*/
 -- 10. What was the volume of orders for each day of the week?
+-- The DAYOFWEEK() function returns the weekday index for a given date ( 1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thursday, 6=Friday, 7=Saturday )
+-- DAYNAME() returns the name of the week day
+
+SELECT dayname(order_time) AS 'Day Of Week',
+       count(order_id) AS 'Number of pizzas ordered',
+       round(100*count(order_id) /sum(count(order_id)) over(), 2) AS 'Volume of pizzas ordered'
+FROM pizza_runner.customer_orders_temp
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- OR
 SELECT 
       dayname(order_time) AS day_of_week,
       count(*) AS number_of_pizzas
 FROM 
-    cleaned_customers_data
+    customer_orders_temp
 GROUP BY 
     day_of_week
 ORDER BY 
